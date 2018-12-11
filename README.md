779706ea428659588f2c445818b8a923469cf141
Luis Donoso <ldonoso@ext.cafpower.com>
2018-12-11 15:27:30 +0100

    Ninth day. Use Data.Sequence.Seq instead of a vanilla List

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ Solutions to [Advent of code 2018](https://adventofcode.com/2018/day/7#part2)
     main "input_test.txt" 2 0
     main "input.txt" 5 60
 
+## Day 9
+
+`Data.Sequence.Seq` was used instead of `[]` so the performance is good enough.
+
+To execute the unit tests:
+
+    Day9.test
+    
+
--- day9/Day9.hs
+++ day9/Day9.hs
@@ -6,77 +6,60 @@ import Data.List
 import Data.Ord (comparing)
 import Text.Pretty.Simple
 import Test.Hspec
+import Utils
+import qualified Data.Sequence as DS
 
 
 parserMarbles :: Parser (Int, Int)
 parserMarbles = do
-    players <- fromInteger <$> integer
+    nPlayers <- fromInteger <$> integer
     string "players; last marble is worth "
     points <- fromInteger <$> integer
     string "points"
-    return (players, points)
+    return (nPlayers, points)
 
 
-data Game = Game { marbles :: [Int], current :: Int, scores :: [(Int, Int)]} deriving (Show)
-
-
-getPos :: Int -> Int -> [Int] -> Int
-getPos current offset xs = (current + offset) `mod` (length $ xs)
+data Game = Game { marbles :: DS.Seq Int, scores :: [(Int, Int)]} deriving (Show)
 
 
 addMarble :: Game -> Int -> Int -> Game
-addMarble (Game marbles current scores) player marble
+addMarble (Game marbles scores) player marble
     | marble `mod` 23 == 0 =
         let 
-            pos = getPos current (-7) marbles
-            (l, r) = splitAt pos marbles
-            (marbles', marbleRemoved) = remove l r
-            scores' = reduceScores $ (player, marble + marbleRemoved) : scores
+            marbles' = rotate (-7) marbles
+            scores' = (player, marble + DS.index marbles' 0) : scores
         in 
-            Game marbles' pos scores'
+            Game (DS.drop 1 marbles') scores'
 
-    | otherwise = 
-        let 
-            pos = getPos current 2 marbles
-            (l, r) = splitAt pos marbles
-        in 
-            Game (l ++ (marble : r)) pos scores
-
-    where
-        remove l (r:rs) = (l ++ rs, r)
+    | otherwise = Game (marble DS.<| rotate 2 marbles) scores
 
 
-getGames :: Game -> Int -> Int -> [Game]
-getGames game players marbles = go game (zip (cycle [1 .. players]) [1 .. marbles]) 
-    where
-        go game [] = []
-        go game ((player, marble):xs) =
-            let game' = addMarble game player marble
-            in game' : go game' xs
+getLastGame :: Game -> [(Int, Int)] -> Game
+getLastGame game moves = foldl' (\g (p, m) -> addMarble g p m) game moves
 
-getLastGame :: Game -> Int -> Int -> Game
-getLastGame game players marbles = go game (zip (cycle [1 .. players]) [1 .. marbles]) 
-    where
-        go game [] = game
-        go game ((player, marble):xs) = go (addMarble game player marble) xs
 
-reduceScores :: [(Int, Int)] -> [(Int, Int)]
+reduceScores :: [(Int, Int)] -> [(Int, Integer)]
 reduceScores =
     fmap (\x -> (fst . head $ x, getTotal x))
     . groupBy (\l r -> fst l == fst r)
     . sortBy (comparing fst)
-    where getTotal = sum . fmap snd
+    where
+        getTotal :: [(Int, Int)] -> Integer
+        getTotal = sum . fmap (toInteger . snd)
 
 
-getWinner :: [(Int, Int)] -> Int
+getWinner :: [(Int, Int)] -> Integer
 getWinner = maximum . fmap snd . reduceScores
 
-play :: Int -> Int -> Int
-play players marbles = 
-    let game = getLastGame (Game [0] 0 []) players marbles
+
+play :: Int -> Int -> Integer
+play nPlayers marbles = 
+    let
+        moves = zip (cycle [1 .. nPlayers]) [1 .. marbles]
+        game = getLastGame (Game (DS.singleton 0) []) moves
     in getWinner $ scores game
 
-        
+
 main :: String -> IO ()
 main filename = do
     inh <- openFile filename ReadMode
@@ -87,23 +70,15 @@ main filename = do
     case x of
         Failure e -> print e
 
-        Success (players, marbles) -> do
+        Success (nPlayers, marbles) -> do
+
             putStr "Fst solution: "
-            print $ play players marbles
+            print $ play nPlayers marbles
+
+            putStr "Snd solution: "
+            print $ play nPlayers (marbles * 100)
 
     hClose inh
-
-
-printAdvance :: (Show a) => Int -> [a] -> IO ()
-printAdvance ctr [] = putStrLn $ "Processed " ++ show ctr
-printAdvance ctr xs = do
-    let step = 5000
-    let (l, r) = splitAt step xs
-    let ctr' = ctr + length l
-    
-    print $ last l
-    putStrLn $ "Processed " ++ show ctr'
-    printAdvance ctr' r
 
 
 test :: IO ()
@@ -111,5 +86,5 @@ test = hspec $ do
     it "Day9 tests" $ do
         play 9 25 `shouldBe` 32
         play 10 1618 `shouldBe` 8317
-        -- play 13 7999 `shouldBe` 146373
-        -- play 466 71436 `shouldBe`
+        play 13 7999 `shouldBe` 146373
+        play 466 71436 `shouldBe` 382055
